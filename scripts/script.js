e7c178bc4e9fcd1d4f68440aac59ffcd4a5ae5eb
nadinemosch <nadine.mosch@hva.nl>
2020-12-12 15:17:52 +0100

    Aanpassingen 12 dec (voornamelijk header)

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+
+// https://codepen.io/shooft/pen/BaKGKwm?editors=1100
+
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  deNav = event.target.parentNode;
+  deNav.classList.toggle("toonMenu");
+}
