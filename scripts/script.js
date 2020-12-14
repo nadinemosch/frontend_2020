b09721e95fa31337585703126b36dfa881865f31
nadinemosch <nadine.mosch@hva.nl>
2020-12-14 20:30:02 +0100

    Updates 14 dec

--- scripts/script.js
+++ scripts/script.js
@@ -8,17 +8,5 @@ deButton.addEventListener("click", toggleMenu);
 
 function toggleMenu(event) {
   deNav = event.target.parentNode;
-  deNav.classList.toggle("hamburgerMenu");
+  deNav.classList.toggle("toonMenu");
 }
-
-// EINDOPDRACHT
-const hamburger = document.querySelector('.hamburger');
-const line = document.querySelector('.line');
-const navigation = document.querySelector('.navigation');
-hamburger.addEventListener('click', animateHamburger);
-const hamburgermenu = document.querySelector ('.hamburgermenu')
-
-function animateHamburger() {
-	hamburger.classList.toggle('active');
-	navigation.classList.toggle('active');
-};
