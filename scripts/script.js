b5bb1338c10ea8201827470717c33dc87f89d259
nadinemosch <nadine.mosch@hva.nl>
2020-12-14 15:29:33 +0100

    main + footer styling

--- scripts/script.js
+++ scripts/script.js
@@ -8,5 +8,17 @@ deButton.addEventListener("click", toggleMenu);
 
 function toggleMenu(event) {
   deNav = event.target.parentNode;
-  deNav.classList.toggle("toonMenu");
+  deNav.classList.toggle("hamburgerMenu");
 }
+
+// EINDOPDRACHT
+const hamburger = document.querySelector('.hamburger');
+const line = document.querySelector('.line');
+const navigation = document.querySelector('.navigation');
+hamburger.addEventListener('click', animateHamburger);
+const hamburgermenu = document.querySelector ('.hamburgermenu')
+
+function animateHamburger() {
+	hamburger.classList.toggle('active');
+	navigation.classList.toggle('active');
+};
