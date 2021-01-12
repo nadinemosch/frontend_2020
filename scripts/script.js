a42c98d462fcc2cc4db6bd25acd109f81941b7fc
nadinemosch <nadine.mosch@hva.nl>
2021-01-12 23:53:02 +0100

    the final countdown pt 2

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,20 @@ function myFunction(smallImg) {
   var fullImg = document.getElementById("imageBox");
   fullImg.src = smallImg.src;
 }
+
+
+var button = document.getElementById('addButton');
+
+button.addEventListener('click', productToevoegen);
+
+function productToevoegen(){
+	button.classList.toggle('welToegevoegd');
+    var isToegevoegd = button.classList.contains('welToegevoegd')
+    
+    if(isToegevoegd){
+        button.textContent='Delete from cart'
+    }
+    else{
+        button.textContent='Add to cart'
+    }
+}
